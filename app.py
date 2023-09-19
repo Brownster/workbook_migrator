759fbc4c03ad839a17b74f22892fe99c448b9546
Marc Brown <marcsmobile.brown@googlemail.com>
2023-09-19 22:02:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,53 @@
+from flask import Flask, render_template, request, flash, redirect, send_from_directory
+import os
+import pandas as pd
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = '123456789'
+
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = {'csv'}
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     download_link = None
 
     if request.method == 'POST':
-        # ... rest of your code ...
+        if 'file1' not in request.files or 'file2' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+
+        if file1.filename == '' or file2.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
 
         if file1 and allowed_file(file1.filename) and file2 and allowed_file(file2.filename):
-            filename1 = None  # Add this line here
+            filename1 = None
             try:
-                # ... rest of your code ...
+                filename1 = secure_filename(file1.filename)
+                filepath1 = os.path.join(app.config['UPLOAD_FOLDER'], filename1)
+                file1.save(filepath1)
+
+                filename2 = secure_filename(file2.filename)
+                filepath2 = os.path.join(app.config['UPLOAD_FOLDER'], filename2)
+                file2.save(filepath2)
+
+                # CSV comparison logic
+                csv1 = pd.read_csv(filepath1)
+                csv2 = pd.read_csv(filepath2)
+
+                combined_csv = csv1.merge(csv2[['ip_address', 'Secret Server URL', 'Configuration Item']], on='ip_address', how='left')
 
                 result_filename = "result.csv"
                 result_filepath = os.path.join(app.config['UPLOAD_FOLDER'], result_filename)
@@ -21,3 +60,18 @@ def index():
                 flash('Error processing files. Please ensure they are valid CSVs.')
 
     return render_template('index.html', download_link=download_link)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename=filename, as_attachment=True)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
